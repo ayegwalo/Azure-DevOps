8227a962ceed16aec4c441b442e751d5e1f30fa6
ayegwalo <chris.relaxier@gmail.com>
2024-08-05 02:11:59 +0400

    Feature 1 added to Program.cs

--- my-project/Program.cs
+++ my-project/Program.cs
@@ -10,6 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // This is very important.
     app.UseHsts();
 }
 
